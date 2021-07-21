667873cb8a2a8348d12dc825b269053232990a16
Alex White <alexlloydwhite@gmail.com>
2021-07-21 11:14:56 -0500

    adding components directory

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "./redux/reducers/_rootReducer";
+import { RootState } from "../../redux/reducers/_rootReducer";
 
 export default function App() {
   const dispatch = useDispatch();
