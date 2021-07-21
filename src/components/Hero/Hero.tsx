99831b22130b578feaf8f3b94a8c7151933077f2
Alex White <alexlloydwhite@gmail.com>
2021-07-21 17:27:52 -0500

    adding imagest to hero

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,32 +2,41 @@
 import {
     Grid,
     Typography,
-    makeStyles
+    makeStyles,
+    Container
 } from '@material-ui/core';
-import PizzaHeroImage from '../Images/PizzaHeroImage.png';
-import TomatoHeroImage from '../Images/TomatoHeroImage.png';
-import GarlicHeroImage from '../Images/GarlicHeroImage.png';
+import PizzaHeroImage from './Images/PizzaHeroImage.png';
+import TomatoHeroImage from './Images/TomatoHeroImage.png';
+import GarlicHeroImage from './Images/GarlicHeroImage.png';
 // Styles
 const useStyles = makeStyles((theme) => ({
     heroBackground: {
-        position: 'relative',
         backgroundColor: '#f0f6fb',
         backgroundSize: 'cover',
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'center',
-        height: 600,
+        height: 550,
+    },
+    container: {
+        display: 'flex',
+    },
+    wrapper: {
+        marginTop: 100,
+        textAlign: 'center',
+    },
+    image: {
+        maxWidth: '100%',
     },
     heroOverlay: {
-        position: 'relative',
-        marginTop: theme.spacing(25),
+        justifyContent: 'center',
+    },
+    pizza: {
+        textAlign: 'center',
+        marginTop: theme.spacing(-10)
     },
     tomato: {
-        height: 200,
-        width: 200
-    },
-    garlic: {
-        height: 180,
-        width: 180
+        width: 250,
+        heigh: 250
     }
 }));
 export default function Hero() {
@@ -35,35 +44,25 @@ export default function Hero() {
 
     return (
         <div className={classes.heroBackground}>
-            <Grid container>
-                <Grid item xs={12}>
-                    <div className={classes.heroOverlay}>
-                        <Typography
-                            variant="h3"
-                            align="center"
-                        >
-                            Pizza Delivery in Minneapolis
-                        </Typography>
-                        <Typography
-                            variant="h5"
-                            align="center"
-                        >
-                            Order delicious, fresh pizza today!
-                        </Typography>
-                    </div>
+            <Container component="section" className={classes.container}>
+                <Grid container>
+                    <Grid item xs={12} className={classes.wrapper} >
+                        <div>
+                            <Typography variant="h3" gutterBottom>
+                                Pizza Delivery in Minneapolis
+                            </Typography>
+                            <Typography variant="h5">
+                                Order Today!
+                            </Typography>
+                        </div>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <div className={classes.wrapper}>
+                            <img src={PizzaHeroImage} className={classes.image} />
+                        </div>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12}>
-                    <div style={{ marginTop: 100, textAlign: 'center' }}>
-                        <img src={PizzaHeroImage} />
-                    </div>
-                </Grid>
-                <Grid item xs={6}>
-                    <img src={TomatoHeroImage} className={classes.tomato} />
-                </Grid>
-                <Grid item xs={6}>
-                    <img src={GarlicHeroImage} className={classes.garlic}/>
-                </Grid>
-            </Grid>
+            </Container>
         </div>
     );
 }
