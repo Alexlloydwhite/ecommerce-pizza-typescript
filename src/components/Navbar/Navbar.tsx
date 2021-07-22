7b9f082ac1d46d75ef53464082ecd51236008dcc
Alex White <alexlloydwhite@gmail.com>
2021-07-21 20:15:24 -0500

    adding image carousel funcionality

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,21 @@
+// MUI
+import {
+    Typography,
+    AppBar,
+    Toolbar
+} from "@material-ui/core";
+
 export default function Navbar() {
     return (
-        <h5>nav baaaarr</h5>
+        <AppBar elevation={0} style={{ marginBottom: 100 }}>
+            <Toolbar>
+                <Typography style={{ flexGrow: 1 }}>
+                    Pizza Time
+                </Typography>
+                <Typography style={{ marginRight: 10, cursor: 'pointer' }}>
+                    Cart
+                </Typography>
+            </Toolbar>
+        </AppBar>
     );
 }
